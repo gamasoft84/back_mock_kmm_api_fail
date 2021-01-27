54ce16ef34367ef8192d9945e9cafc6e3c6b42bb
Richard Gama <rgamag128@gmail.com>
2021-01-27 12:01:17 -0600

    add router

--- src/routes/mock.js
+++ src/routes/mock.js
@@ -1,13 +1,32 @@
 const { Router } = require('express');
 const router = Router();
 
-const { RequestPOVINdata, SubmitCustomerInformation } = require('../controllers/mock.controller');
+const { RequestPOVINdata, SubmitCustomerInformation, SubmitCheckinLeadData, SubmitTestDriveFeedback, SubmitPOVINdata, SubmitVehicleDetailData, SubmitStaffData, SubmitInvoiceVINdata } = require('../controllers/mock.controller');
 
 router.route('/RequestPOVINdata')
     .post(RequestPOVINdata);
 
 router.route('/SubmitCustomerInformation')
     .post(SubmitCustomerInformation);
+
+router.route('/SubmitCheckinLeadData')
+    .post(SubmitCheckinLeadData);
+
+router.route('/SubmitTestDriveFeedback')
+    .post(SubmitTestDriveFeedback);
+
+router.route('/SubmitPOVINdata')
+    .post(SubmitPOVINdata);
+
+router.route('/SubmitInvoiceVINdata')
+    .post(SubmitInvoiceVINdata);
+
+router.route('/SubmitVehicleDetailData')
+    .post(SubmitVehicleDetailData);
+
+router.route('/SubmitStaffData')
+    .post(SubmitStaffData);
+
 
 
 module.exports = router;
